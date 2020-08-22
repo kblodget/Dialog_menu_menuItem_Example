a50adeddda7243e499fc159dcf92aefbca559a6d
kblodget <memfundraiser2010@gmail.com>
2020-08-22 08:18:30 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,57 +4,128 @@ import TestDialog from "./TestDialog.js";
 import UnsubscribePopup from "./UnsubscribePopup.js";
 import { Button, Menu, MenuItem } from "@material-ui/core";
 
-export default function App() {
-  const [menuEl, setMenuEl] = React.useState(null);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuEl: null,
+      unsubscribeDialogOpen: false,
+      actionKey: null
+    };
+  }
 
-  const handleClick = (event) => {
-    setMenuEl(event.currentTarget);
+  setMenuEl = (target) => {
+    this.setState({
+      menuEl: target
+    });
   };
 
-  const handleClose = () => {
+  handleClick = (event) => {
+    this.setMenuEl(event.currentTarget);
+  };
+
+  handleClose = () => {
     console.log("in handleClose...");
-    setMenuEl(null);
+    this.setMenuEl(null);
   };
 
-  return (
-    <div className="App">
-      <div tabIndex="0">Click the button, to display menu</div>
-      <Button
-        data-testid={"actionMenu"}
-        disabled={false}
-        onClick={handleClick}
-        size={"medium"}
-        variant={"contained"}
-      >
-        <span>Show Menu</span>
-      </Button>
+  openDialog = () => {
+    this.setState({
+      unsubscribeDialogOpen: true,
+      actionKey: ""
+    });
+  };
 
-      <Menu
-        anchorEl={menuEl}
-        open={Boolean(menuEl)}
-        keepMounted
-        onClose={handleClose}
-        getContentAnchorEl={null}
-        autoFocus={false}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "left"
-        }}
-      >
-        <MenuItem
-          children="Delete"
-          component={TestDialog}
-          button={true}
-          onClick={handleClose}
+  closeUnsubscribeDialog = () => {
+    this.setState({
+      unsubscribeDialogOpen: false,
+      actionKey: null
+    });
+  };
+
+  confirmUnsubscribe = () => {
+    console.log("in confirmUnsubscribe ...");
+    this.closeUnsubscribeDialog();
+  };
+
+  /*
+  openConfirmationDialog = (actionStatus) => {
+    this.setState({
+      unsubscribeDialogOpen: true,
+    });
+  }
+  */
+
+  openConfirmationDialog = (actionStatus) => {
+    return () => {
+      if (actionStatus === "create") {
+        alert("create stuff here");
+      } else {
+        this.setState({
+          ...this.state,
+          unsubscribeDialogOpen: true,
+          actionKey: actionStatus
+        });
+      }
+    };
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div tabIndex="0">Click the button, to display menu or Dialog</div>
+        <Button
+          data-testid={"actionMenu"}
+          disabled={false}
+          onClick={this.handleClick}
+          size={"medium"}
+          variant={"contained"}
+        >
+          <span>Show Menu</span>
+        </Button>
+
+        <Menu
+          anchorEl={this.state.menuEl}
+          open={Boolean(this.state.menuEl)}
+          keepMounted
+          onClose={this.handleClose}
+          getContentAnchorEl={null}
+          autoFocus={false}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left"
+          }}
+        >
+          <MenuItem
+            children="Delete"
+            component={TestDialog}
+            button={true}
+            onClick={this.handleClose}
+          />
+          <MenuItem
+            children="Unsubscribe"
+            button={true}
+            onClick={this.openConfirmationDialog()}
+          />
+        </Menu>
+        <Button
+          data-testid={"actionDialog"}
+          disabled={false}
+          onClick={this.openConfirmationDialog("owner_delete")}
+          size={"medium"}
+          variant={"contained"}
+        >
+          <span>Show Dialog</span>
+        </Button>
+        <UnsubscribePopup
+          closeUnsubscribeDialog={this.closeUnsubscribeDialog}
+          unsubscribeDialogOpen={this.state.unsubscribeDialogOpen}
+          selectedSubscriptionName={"subscription Name"}
+          deleteAction={this.state.actionKey}
+          confirmUnsubscribe={this.confirmUnsubscribe}
         />
-        <MenuItem
-          children="Unsubscribe"
-          component={TestDialog}
-          button={true}
-          onClick={handleClose}
-          s
-        />
-      </Menu>
-    </div>
-  );
+      </div>
+    );
+  }
 }
+export default App;
